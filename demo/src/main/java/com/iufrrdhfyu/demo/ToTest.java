37df014a04d23f5bcf50563cb7f246597e560151
CJjos <134951932+CJjos@users.noreply.github.com>
2023-11-02 21:43:27 +0800

    Add files via upload

---
+++ demo/src/main/java/com/iufrrdhfyu/demo/ToTest.java
@@ -0,0 +1,7 @@
+package com.iufrrdhfyu.demo;
+
+public class ToTest {
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
---
+++ demo/src/test/java/TestToTest.java
@@ -0,0 +1,9 @@
+import com.iufrrdhfyu.demo.ToTest;
+import org.junit.jupiter.api.Test;
+
+public class TestToTest {
+    @Test
+    public void test01(){
+        System.out.println(ToTest.add(1, 32));
+    }
+}
