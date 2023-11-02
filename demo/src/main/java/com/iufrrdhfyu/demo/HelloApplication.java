ab8d586fa633cf525471ce09d1c43a3aeedb7e5d
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2023-11-02 22:10:21 +0800

    dy modified source code 2023:11:02 22:10

--- demo/src/main/java/com/iufrrdhfyu/demo/HelloApplication.java
+++ demo/src/main/java/com/iufrrdhfyu/demo/HelloApplication.java
@@ -3,6 +3,7 @@ package com.iufrrdhfyu.demo;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
+// 这是我李四修改的这一部分源码
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
 
