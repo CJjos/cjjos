99bfe253b3c76663536eba58ab9b9529526531bd
David Antillies <93983691+dongyan3721@users.noreply.github.com>
2023-11-02 23:22:22 +0800

    dy added new fun 2023:11:02 23:22

--- demo/src/main/java/com/iufrrdhfyu/demo/HelloApplication.java
+++ demo/src/main/java/com/iufrrdhfyu/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
 // 这是我李四修改的这一部分源码
+// 这是我李四加的新功能
 @ApplicationPath("/api")
 public class HelloApplication extends Application {
 
